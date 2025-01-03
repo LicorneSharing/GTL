477fce17dd5c88d18847271f196fbcbb02370823
Florent Carrez <florent.carrez.fc@gmail.com>
2025-01-04 00:37:17 +0100

    add MapRef function

--- slices/functions.go
+++ slices/functions.go
@@ -1,10 +1,16 @@
 package slices
 
 func Map[T any, U any, S ~[]T](slice S, fn func(T) U) []U {
+	return MapRef(slice, func(e *T) U {
+		return fn(*e)
+	})
+}
+
+func MapRef[E, U any, S ~[]E](slice S, fn func(*E) U) []U {
 	result := make([]U, len(slice))
 
-	for i, elem := range slice {
-		result[i] = fn(elem)
+	for i := range slice {
+		result[i] = fn(&slice[i])
 	}
 
 	return result
